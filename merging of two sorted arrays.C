1aea61ea331139d2ae9953e66d6422dc22a12442
2015jamrajput <92567841+2015jamrajput@users.noreply.github.com>
2021-10-18 08:53:05 +0530

    Update merging of two sorted arrays.C

--- merging of two sorted arrays.C
+++ merging of two sorted arrays.C
@@ -1,52 +1,52 @@
-#include<stdio.h>
-int main(){
-    int n1,n2,i,j;
-    printf("Enter the length of First Sorted Array:-\n");
+#include <stdio.h>
+
+int main()
+{
+    
+    int n1,n2,n3;            //Array Size Declaration
+    printf("\nEnter the size of first array ");
     scanf("%d",&n1);
-    int sort1[n1];
-    printf("Enter the First array elements in incresing order:-\n");
-    for(i=0;i<n1;i++){
-        scanf("%d",&sort1[i]);
-    }
-    printf("Enter the length of Second Sorted Array:-\n");
+    printf("\nEnter the size of second array ");
     scanf("%d",&n2);
-    int sort2[n2];
-    printf("Enter the Second array elements in incresing order:-\n");
-    for(i=0;i<n2;i++){
-        scanf("%d",&sort2[i]);
+    
+    n3=n1+n2;
+    printf("\nEnter the sorted array elements");
+    int a[n1],b[n2],c[n3];     //Array Declaration
+    for(int i=0;i<n1;i++)      //Array Initialized
+    {
+       scanf("%d",&a[i]);
+       c[i]=a[i];
     }
-    i=0;
-    j=0;
-    int k=0;
-    int n=(n1+n2);
-    int res[n];
-    while(i<n1 && j<n2){
-        if(sort1[i]<=sort2[j]){
-            res[k]=sort1[i];
-            k++;
-            i++;
-        }else{
-            res[k]=sort2[j];
-            k++;
-            j++;
+    int k=n1;
+    printf("\nEnter the sorted array elements");
+    for(int i=0;i<n2;i++)      //Array Initialized
+    {
+        scanf("%d",&b[i]);
+        c[k]=b[i];
+        k++;
+    }
+    printf("\nThe merged array..\n");
+    for(int i=0;i<n3;i++)
+    printf("%d ",c[i]);        //Print the merged array
+    
+    printf("\nAfter sorting...\n");
+    for(int i=0;i<n3;i++)         //Sorting Array
+    {
+        int temp;
+        for(int j=i+1; j<n3 ;j++)
+        {
+            if(c[i]<c[j])
+            {
+                temp=c[i];
+                c[i]=c[j];
+                c[j]=temp;
+            }
         }
+    }   
+    
+    for(int i=0 ; i<n3 ; i++)       //Print the sorted Array 
+    {
+        printf(" %d ",c[i]);
     }
-    if(i>=n1){
-        while(j<n2){
-            res[k]=sort2[j];
-            k++;
-            j++;
-        }
-    }else{
-        while(i<n1){
-            res[k]=sort1[i];
-            k++;
-            i++;
-        }
-    }
-    printf("Merged Sorted Array is:-\n");
-    for(i=0;i<k;i++){
-        printf("%d ",res[i]);
-    }
-    return 0;
+    return 0;   
 }
